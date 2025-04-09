7f2aba7b83615362cad59d4b0faa7fa8ffedbb2a
Jonah So <jonahso@ucsb.edu>
2025-04-09 15:16:59 -0700

    equals team test

--- src/test/java/edu/ucsb/cs156/spring/hello/HelloControllerTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/HelloControllerTest.java
@@ -64,5 +64,6 @@ public class HelloControllerTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    
 
 }
--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,7 +1,10 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,12 +18,44 @@ public class TeamTest {
     }
 
     @Test
-    public void getName_returns_correct_name() {
+    public void getName_returns_correct_name() 
+    {
        assert(team.getName().equals("test-team"));
     }
 
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() 
+    {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void same_equals_correct() 
+    {
+        Team teamA = new Team("TeamA");
+        teamA.addMember("member1"); 
+        teamA.addMember("member2"); 
+        assertTrue(teamA.equals(teamA));
+        
+        Team sameteam = new Team("TeamA");
+        sameteam.addMember("member1"); 
+        sameteam.addMember("member2"); 
+
+        assertTrue(teamA.equals(sameteam));
+        assertTrue(sameteam.equals(teamA));
+        
+        Object obj = new Object();
+        assertFalse(teamA.equals(obj));
+
+        Team differentTeam = new Team("differentTeam"); 
+        assertFalse(teamA.equals(differentTeam));
+
+        differentTeam.addMember("member3"); 
+        differentTeam.addMember("member4"); 
+        assertFalse(team.equals(differentTeam));
+    }
 
 }
